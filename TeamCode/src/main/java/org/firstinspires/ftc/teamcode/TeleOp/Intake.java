fd9fad3242a0b055cfb514c66dbe56f06a5a060a
Nikhil <nsarathy3@gmail.com>
2025-10-12 20:20:14 -0500

    Fixed the first part of intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Intake.java
@@ -1,28 +1,136 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
+
+
+
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+
+import org.firstinspires.ftc.teamcode.EnhancedFunctions_SELECTED.BasicVeloMotor;
 import org.firstinspires.ftc.teamcode.EnhancedFunctions_SELECTED.BetterGamepad;
-import org.firstinspires.ftc.teamcode.util.PIVoltageMotor;
 import org.firstinspires.ftc.teamcode.util.Subsystem;
 
+
 public class Intake extends Subsystem {
-//Initialized sub-systems
-    private PIVoltageMotor intake;
-
-    private BetterGamepad gamepad1;
 
 
+    private BetterGamepad controller;
 
-    public void provideComponents(PIVoltageMotor intake, BetterGamepad gamepad1) {
+
+    private BasicVeloMotor intake;
+    private BasicVeloMotor transfer;
+
+
+    // private DigitalChannel beam_break;
+
+
+    public void provideComponents(BasicVeloMotor intake, BasicVeloMotor intakeBelt, BetterGamepad controller) {
+        this.controller = controller;
         this.intake = intake;
-        this.gamepad1 = gamepad1;
     }
+
+
+
+
+
 
     @Override
     public void update() {
+        intake.setVelocity(800);
+        transfer.setVelocity(600);
+
+
+        try {
+            wait(50);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        intake.setVelocity(0);
+        transfer.setVelocity(0);
+
 
 
 
 
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/V2TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/V2TeleOp.java
@@ -1,58 +1,78 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 
 import org.firstinspires.ftc.teamcode.EnhancedFunctions_SELECTED.BetterGamepad;
 import org.firstinspires.ftc.teamcode.EnhancedFunctions_SELECTED.TeleOpBaseOpMode;
 import org.firstinspires.ftc.teamcode.EnhancedFunctions_SELECTED.TickrateChecker;
 import org.firstinspires.ftc.teamcode.util.RobotResetter;
 
+
 @TeleOp (name = "V2TeleOp")
 public class V2TeleOp extends TeleOpBaseOpMode {
+
 
     private BetterGamepad gamepad1;
     private BetterGamepad gamepad2;
 
+
     private Intake intake = new Intake();
+
 
     @Override
     public void runOpMode() {
 
+
         initializeDevices();
+
 
         gamepad1 = new BetterGamepad(super.gamepad1);
         gamepad2 = new BetterGamepad(super.gamepad2);
 
+
         applyComponentTraits();
 
+
         //initialize subsystems here
-        intake.provideComponents(super.intake, gamepad1);
+
 
         //setup lynx module
         setUpLynxModule();
 
+
         if (isStopRequested()) return;
         waitForStart();
+
 
         //run robot reset
         RobotResetter robotReset = new PostAutonomousRobotReset();
 
+
         while (opModeIsActive()) {
 
+
             TickrateChecker.startOfLoop();
+
 
             // clear data at start of loop
             clearCacheOfLynxModule();
 
+
             gamepad1.getInformation();
             gamepad2.getInformation();
 
+
             intake.update();
+
 
             TickrateChecker.endOfLoop();
 
+
             telemetry.addData("Tick rate", TickrateChecker.getTimePerTick());
             telemetry.update();
+
 
             if(isStopRequested()) {
                 //end
@@ -61,3 +81,4 @@ public class V2TeleOp extends TeleOpBaseOpMode {
         }
     }
 }
+
