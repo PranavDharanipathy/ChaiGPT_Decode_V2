0df24da8dba9470569ae2c068062905d8d069cd5
Nikhil <nsarathy3@gmail.com>
2025-10-14 19:57:44 -0500

    Added Intake.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/WaitCommand.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.util.Command;
+
+public abstract class WaitCommand implements Command {
+
+
+    // ✅ Custom wait function
+    public static void Wait(long ms) {
+        long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < ms) {
+            // do nothing/wait
+        }
+    }
+}
