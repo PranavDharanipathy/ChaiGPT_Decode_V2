2f94bfb8accaa32d992e22b30109c9bdcc8ee5ab
Nikhil <nsarathy3@gmail.com>
2025-10-25 23:28:26 -0500

    Added start code for auton, just need to make the robot move forward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/V2Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/V2Auton.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Auton;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -10,8 +11,11 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+
 @Autonomous(name = "V2Auton")
 public class V2Auton {
+
+    MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
 
     CommandScheduler scheduler = new CommandScheduler();
 
@@ -21,6 +25,9 @@ public class V2Auton {
         //example: drive forward, wait one second(not obstructing drive forward), and then drive back.
 
         //first motor --> drive forward
+
+        drive.setDrivePowers();
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
         //wait 1 sec
         scheduler.add(new WaitCommand(1000));
