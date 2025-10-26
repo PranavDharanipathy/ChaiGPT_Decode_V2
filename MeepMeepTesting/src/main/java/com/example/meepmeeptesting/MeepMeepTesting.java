1801ac1eb04209f69339eff605048081b864dbac
Nikhil <nsarathy3@gmail.com>
2025-10-26 18:40:47 -0500

    Created MeepMeepTesting.java, and added configurations for this code
    Pranav Translator: "tHiS iS aUrA"

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,4 +1,47 @@
 package com.example.meepmeeptesting;
 
+
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
 public class MeepMeepTesting {
+    public static void main(String[] args) {
+        
+        
+        MeepMeep meepMeep = new MeepMeep(800);
+        Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
+
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        /* IF YOU WANT TO ADD ANOTHER BOT
+                RoadRunnerBotEntity mySecondBot = new DefaultBotBuilder(meepMeep)
+                // We set this bot to be red
+                .setColorScheme(new ColorSchemeRedDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+         */
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+                .lineToX(12)
+                .setTangent(Math.toRadians(-90))
+                .lineToY(14)
+                .strafeTo(new Vector2d(44.5, 30))
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+
+    }
 }
