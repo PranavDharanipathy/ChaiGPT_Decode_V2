6ea5cbc13666e943caded4092f5ede3cd3bccf3a
Nikhil <nsarathy3@gmail.com>
2025-11-07 17:24:18 -0600

    Final version for now

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/V2AutonLONG_Blue.java
@@ -0,0 +1,142 @@
+package org.firstinspires.ftc.teamcode.Auton;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ProfileAccelConstraint;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.EnhancedFunctions_SELECTED.AutonomousBaseOpMode;
+import org.firstinspires.ftc.teamcode.ShooterSystems.ShooterInformation;
+import org.firstinspires.ftc.teamcode.TeleOp.Intake;
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+
+import org.firstinspires.ftc.teamcode.Constants;
+
+@Autonomous (name = "V2Auton(BLUE)", group = "AAAA_MatchPurpose")
+public class V2AutonLONG_Blue extends AutonomousBaseOpMode {
+
+
+    public class RobotElements {
+        public InstantAction setFlywheelToFarSideVelocity() {
+            Intake intake = new Intake();
+            return new InstantAction(() -> flywheel.setVelocity(ShooterInformation.ShooterConstants.FAR_SIDE_FLYWHEEL_SHOOT_VELOCITY, true));
+        }
+        public InstantAction stopFlywheel() {
+            return new InstantAction(() -> flywheel.setVelocity(0, true));
+        }
+
+        //transfer
+        public InstantAction antiTransfer() {
+            return new InstantAction(() -> transfer.setVelocity(Constants.ANTI_TRANSFER_VELOCITY));
+        }
+        public InstantAction transferArtifact() {
+            return new InstantAction(() -> transfer.setVelocity(Constants.TRANSFER_VELOCITY));
+        }
+        //intake
+        public InstantAction reverseIntake() {
+            return new InstantAction(() -> intake.setVelocity(Constants.REVERSE_INTAKE_VELOCITY));
+        }
+
+        public InstantAction intake() {
+            return new InstantAction(() -> intake.setVelocity(Constants.BASE_INTAKE_VELOCITY));
+        }
+
+    }
+    public double turretStartPosition;
+
+    @Override
+    public void runOpMode() {
+        fullInit();
+        final RobotElements robot = new RobotElements();
+        turretStartPosition = turret.getCurrentPosition();
+
+        telemetry.addData("turret current position", turretStartPosition);
+        telemetry.update();
+
+        ElapsedTime timer = new ElapsedTime();
+
+        int startPosition = 0;
+        telemetry.addData("Starting Position", startPosition);
+        telemetry.update();
+
+        Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(0));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        Action shootPreloadedArtifacts =
+
+                new SequentialAction(
+                        new ParallelAction(
+                                robot.intake(),
+                                robot.transferArtifact()
+                        ),
+                        new SleepAction(1),
+
+                        new ParallelAction(
+                                robot.intake(),
+                                robot.transferArtifact()
+                        ),
+                        new SleepAction(1),
+
+                        new ParallelAction(
+                                robot.intake(),
+                                robot.transferArtifact()
+                        )
+                );
+
+        Action mainPath =
+                //MOVE TO FIRST INTAKE POINT
+
+                new ParallelAction(
+                        robot.intake(),
+                        drive.actionBuilder(initialPose)
+
+                                //TANGENT = 90
+                                //FIRST INTAKE
+                                .splineTo(new Vector2d(21, 42), Math.PI / 2,
+                                        new TranslationalVelConstraint(50), new ProfileAccelConstraint(-50, 50))
+                                //GO TO SMALL TRIANGLE
+                                .setReversed(true)
+                                .splineToSplineHeading(new Pose2d(20, 8, Math.PI), Math.PI / 2,
+                                        new TranslationalVelConstraint(50), new ProfileAccelConstraint(-50, 50))
+                                .waitSeconds(2)
+
+                                //MOVE TO 2nd INTAKE POINT
+
+                                .setReversed(false)
+
+                                .splineToSplineHeading(new Pose2d(20, 8, Math.PI / 2), Math.PI,
+                                        new TranslationalVelConstraint(50), new ProfileAccelConstraint(-50, 50))
+
+                                .splineToConstantHeading(new Vector2d(48, 49), Math.PI / 2,
+                                        new TranslationalVelConstraint(50), new ProfileAccelConstraint(-50, 50))
+                                //.splineTo(new Vector2d(44, 47), Math.PI / 2)
+
+                                //GO TO SMALL TRIANGLE
+                                .setReversed(true)
+                                .splineToSplineHeading(new Pose2d(18, 2, Math.PI), Math.PI / 2,
+                                        new TranslationalVelConstraint(50), new ProfileAccelConstraint(-50, 50))
+
+                                .build()
+                );
+
+
+
+
+        waitForStart();
+
+
+
+        Actions.runBlocking(new SequentialAction(mainPath));
+        //SHOOT!
+        telemetry.addData("flywheel speed", flywheel.getFrontendCalculatedVelocity());
+        telemetry.update();
+    }
+}
