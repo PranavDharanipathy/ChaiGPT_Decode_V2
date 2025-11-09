899902f67a2d9f050a993ad3d1fd77063f1af648
Nikhil <nsarathy3@gmail.com>
2025-11-08 21:09:18 -0600

    Started red auton, need to fix going back to small triangle + second intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/V2AutonLONG_RED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/V2AutonLONG_RED.java
@@ -15,10 +15,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.EnhancedFunctions_SELECTED.AutonomousBaseOpMode;
 import org.firstinspires.ftc.teamcode.ShooterSystems.ShooterInformation;
-import org.firstinspires.ftc.teamcode.TeleOp.Intake;
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
-@Autonomous (name = "V2Auton(RED)", group = "AAAA_MatchPurpose",  preselectTeleOp = "V2TeleOp_RED")
-public class V2AutonRed extends AutonomousBaseOpMode {
+@Autonomous (name = "V2AutonLONG(RED)", group = "AAAA_MatchPurpose",  preselectTeleOp = "V2TeleOp_RED")
+public class V2AutonLONG_RED extends AutonomousBaseOpMode {
 
     public class RobotElements {
 
@@ -68,50 +67,64 @@ public class V2AutonRed extends AutonomousBaseOpMode {
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
 
-        Action first_intake =
+        Action mainPath =
+                //GO TO FIRST INTAKE(RED)
+
                 new ParallelAction(
                         robot.intake(),
-                        drive.actionBuilder(initialPose)
-                                .splineTo(new Vector2d(28.5, 44), -90)
-                                .build()
-                );
 
-        Action goal =
-                new SequentialAction(
+                        //GO TO FIRST INTAKE(RED)
                         drive.actionBuilder(initialPose)
+                                .splineTo(new Vector2d(33, -43), -Math.PI / 2,
+                                        new TranslationalVelConstraint(70), new ProfileAccelConstraint(-50, 50))
+
+                        //GO TO SMALL TRIANGLE
+                                //.splineToSplineHeading(new Pose2d(22, 4, -180), Math.toRadians(-90))
+
                                 .setReversed(true)
 
-                                //TODO: MAke the spline angle(reverse) more accurate
-                                .splineTo(new Vector2d(-3, 1), Math.toRadians(0))
-                                .build(),
-                        new ParallelAction(
-                                new InstantAction(() -> hoodAngler.setPosition(ShooterInformation.ShooterConstants.HOOD_FAR_POSITION)),
-                                robot.transferArtifact(),
-                                robot.setFlywheelToFarSideVelocity()
-                        )
-                );
+                                .splineToSplineHeading(new Pose2d(25, -21, Math.toRadians(-145)), -Math.PI / 2,
+                                        new TranslationalVelConstraint(95), new ProfileAccelConstraint(-50, 50))
+                                .splineToSplineHeading(new Pose2d(22, 4, -Math.PI), -Math.PI / 2,
+                                        new TranslationalVelConstraint(60), new ProfileAccelConstraint(-33, 33))
 
-        Action second_intake =
-                new ParallelAction(
-                        drive.actionBuilder(initialPose)
-                                .splineTo(new Vector2d(52, 45), -90)
-                                .build()
-                );
+                                .waitSeconds(5)
 
-        Action third_intake =
-                new ParallelAction(
-                        drive.actionBuilder(initialPose)
-                                .splineTo(new Vector2d(73, 43), -90)
+
+
+                                //GO TO SECOND INTAKE(RED)
+                                //52, -44
+                                .splineToSplineHeading(new Pose2d(38, -30, Math.toRadians(135)), Math.toRadians(180))
+                                .splineToSplineHeading(new Pose2d(52, -44, Math.toRadians(45)), Math.toRadians(45))
+
+
+                                //GO TO SMALL TRIANGLE
+                                .splineToSplineHeading(new Pose2d(30, -16, Math.toRadians(-135)), Math.PI / 2,
+                                        new TranslationalVelConstraint(95), new ProfileAccelConstraint(-50, 50))
+                                .splineToSplineHeading(new Pose2d(22, -16, -Math.PI), -Math.PI / 2,
+                                        new TranslationalVelConstraint(60), new ProfileAccelConstraint(-33, 33))
+
+                                .waitSeconds(5)
+
+
+
+
+
+
+
+
+
+
+
                                 .build()
+
                 );
 
         waitForStart();
 
         if (isStopRequested()) return;
 
-
-
-        Actions.runBlocking(new SequentialAction(first_intake, goal, second_intake, goal));
+        Actions.runBlocking(new SequentialAction(mainPath));
         //SHOOT!
         telemetry.addData("flywheel speed", flywheel.getFrontendCalculatedVelocity());
         telemetry.update();
