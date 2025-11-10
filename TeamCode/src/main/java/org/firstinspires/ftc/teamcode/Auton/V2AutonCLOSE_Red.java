408d3f48581affd9f4212e537ee4705412524a8e
Nikhil <nsarathy3@gmail.com>
2025-11-09 19:27:47 -0600

    CLOSE v1.0 - CLOSE autons created + values added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/V2AutonCLOSE_Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/V2AutonCLOSE_Red.java
@@ -5,27 +5,25 @@ import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.EnhancedFunctions_SELECTED.AutonomousBaseOpMode;
 import org.firstinspires.ftc.teamcode.ShooterSystems.ShooterInformation;
+import org.firstinspires.ftc.teamcode.TeleOp.Intake;
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 
-@Autonomous(name = "V2AutonCLOSE_Blue", group = "AAAA_MatchPurpose", preselectTeleOp = "V2Teleop_BLUE")
-public class V2AutonCLOSE_Blue extends AutonomousBaseOpMode {
+public class V2AutonCLOSE_Red extends AutonomousBaseOpMode {
 
     public class RobotElements {
-
         public InstantAction setFlywheelToFarSideVelocity() {
-
+            Intake intake = new Intake();
             return new InstantAction(() -> flywheel.setVelocity(ShooterInformation.ShooterConstants.FAR_SIDE_FLYWHEEL_SHOOT_VELOCITY, true));
         }
         public InstantAction stopFlywheel() {
             return new InstantAction(() -> flywheel.setVelocity(0, true));
         }
+
         //transfer
         public InstantAction antiTransfer() {
             return new InstantAction(() -> transfer.setVelocity(Constants.ANTI_TRANSFER_VELOCITY));
@@ -37,15 +35,18 @@ public class V2AutonCLOSE_Blue extends AutonomousBaseOpMode {
         public InstantAction reverseIntake() {
             return new InstantAction(() -> intake.setVelocity(Constants.REVERSE_INTAKE_VELOCITY));
         }
+
         public InstantAction intake() {
             return new InstantAction(() -> intake.setVelocity(Constants.BASE_INTAKE_VELOCITY));
         }
 
     }
+
     public double turretStartPosition;
 
     @Override
     public void runOpMode() throws InterruptedException {
+
 
         fullInit();
 
@@ -60,59 +61,47 @@ public class V2AutonCLOSE_Blue extends AutonomousBaseOpMode {
         telemetry.addData("Starting Position", startPosition);
         telemetry.update();
 
-        Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(-45));
+        Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(0));
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
         Action mainPath =
+
                 new ParallelAction(
-                        robot.intake(),
                         drive.actionBuilder(initialPose)
-                               /// TODO: 11/9/2025 MAKE PRELOAD SPLINE UPDATE: CHECK PRELOAD
-                                //PRELOAD SPLINE(Goes to middle of big triangle)
+                                //PRELOAD(big triangle, red zone)
+                                .splineToSplineHeading(new Pose2d(36, 54, 90), 45)
 
-                                .splineToSplineHeading(new Pose2d(43, -51, -45), Math.toRadians(-90))
+                                //FIRST INTAKE
 
-                               //FIRST INTAKE(-42, 0)
+                                .splineToSplineHeading(new Pose2d(-45, -1, 180), Math.toRadians(90))
 
-                                //.splineTo(new Vector2d(-21, -27), -Math.PI / 2)
-                                .splineTo(new Vector2d(-42, 0), -Math.PI / 2)
+                                //big triangle(red zone)
 
-                                //GO TO shooting spot(big triangle)(-35, -64)
-                                .splineToSplineHeading(new Pose2d(-35, -64, -45), Math.toRadians(-90))
+                                .setReversed(true)
 
-                                .waitSeconds(4)
+                                .splineToSplineHeading(new Pose2d(36, 54, 90), 180)
 
-                                //SECOND INTAKE(-65, 0)
-                                .splineTo(new Vector2d(-65, 0), -45)
 
-                                //GO TO shooting spot(big triangle)
+                                //SECOND INTAKE
 
-                                .splineToSplineHeading(new Pose2d(-35, -64, -45), Math.toRadians(-90))
-                                .waitSeconds(4)
+                                .setReversed(false)
 
-                                //THIRD INTAKE
+                                .splineTo(new Vector2d(-70, -2), Math.toRadians(90))
 
+                                .setReversed((true))
+
+                                .splineToSplineHeading(new Pose2d(36, 54, 90), 180)
 
 
 
 
 
-                                .build()
 
+                                        .build()
                 );
 
 
-                waitForStart();
-
-        if (isStopRequested()) return;
 
 
-
-        Actions.runBlocking(new ParallelAction(robot.antiTransfer(), mainPath));
-        //SHOOT!
-        telemetry.addData("flywheel speed", flywheel.getFrontendCalculatedVelocity());
-        telemetry.update();
     }
-
 }
-
