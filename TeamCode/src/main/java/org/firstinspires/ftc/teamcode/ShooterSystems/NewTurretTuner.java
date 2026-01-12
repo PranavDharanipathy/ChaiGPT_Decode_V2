c51e8ee9ff37d15734c4436a4a61fcb3419767c5
Nikhil <nsarathy3@gmail.com>
2026-01-11 18:44:17 -0600

    Created new turret tuner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterSystems/NewTurretTuner.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.ShooterSystems;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.TeleOp.LaNikhilBaseOpMode;
+
+public class NewTurretTuner extends OpMode {
+
+    NewTurret turret;
+
+    Pose2d initialPose;
+    public  static double kp, ki, kd;
+
+
+
+
+    @Override
+    public void init() {
+        initialPose = new Pose2d(0, 0, 0);
+        turret = new NewTurret(hardwareMap, initialPose, 50, 50);
+    }
+
+    @Override
+    public void loop() {
+
+        turret.setPID(kp, ki, kd);
+
+        turret.update();
+
+    }
+}
